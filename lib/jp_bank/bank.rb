3a43130d30942606a2063f61ecc5d7d3bf4619c1
Tatsuya Sato <satoryu.1981@gmail.com>
2014-02-26 23:12:00 +0900

    JpBank::Bank.find responds Bank data for a given bank code

--- lib/jp_bank/bank.rb
+++ lib/jp_bank/bank.rb
@@ -1,11 +1,44 @@
 module JpBank
   class Bank
-    def self.find(code)
-      self.new
+    def self.fetch_bank_from_code(code)
+      self.load_banks unless @bank_codes
+      @bank_codes[code]
+    end
+
+    def self.find(code_or_name)
+      self.fetch_bank_from_code(code_or_name)
+    end
+
+    def initialize(data)
+      @data = data
+    end
+
+    def code
+      @data['code']
     end
 
     def name
-      '楽天銀行' # Fake
+      @data['name']
+    end
+
+    private 
+    def self.load_banks
+      require 'yaml' 
+
+      @banks = data_file_paths.inject([]) do |data, data_path|
+        data + YAML.load_file(data_path)
+      end.map { |data| new(data) }
+      @bank_codes = {}
+      @banks.each do |bank|
+        @bank_codes[bank.code] = bank
+        @bank_codes[bank.code.to_i] = bank
+      end
+      @bank_names = {}
+      @banks.each { |bank| @bank_names[bank.name] = bank }
+    end
+
+    def self.data_file_paths
+      [ File.expand_path('../../data/banks.yml', __FILE__) ]
     end
   end
 end
--- spec/jp_bank/bank_spec.rb
+++ spec/jp_bank/bank_spec.rb
@@ -8,6 +8,16 @@ describe JpBank::Bank do
       let(:bank_code) { 36 }
       
       specify 'load the bank information specified by the given code' do
+        expect(bank.code).to eq('0036')
+        expect(bank.name).to eq('楽天銀行') 
+      end
+    end
+
+    context 'When bank code given as String' do
+      let(:bank_code) { '0036' }
+      
+      specify 'load the bank information specified by the given code' do
+        expect(bank.code).to eq('0036')
         expect(bank.name).to eq('楽天銀行') 
       end
     end
