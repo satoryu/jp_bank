f0fbf8972749372304bbbd88383d0480ed78eef7
Tatsuya Sato <satoryu.1981@gmail.com>
2014-02-28 19:56:39 +0900

    JpBank module provides jp_bank method for defining bank info method

--- lib/jp_bank.rb
+++ lib/jp_bank.rb
@@ -2,4 +2,20 @@ require "jp_bank/version"
 
 module JpBank
   autoload :Bank, File.expand_path('../jp_bank/bank', __FILE__)
+
+  def self.included(base)
+    base.extend JpBank::ClassMethods
+  end
+
+  module ClassMethods
+    def jp_bank(attr_name, options={})
+      bank_code_method = options[:bank_code] || :bank_code
+
+      self.class_eval do
+        define_method attr_name do
+          JpBank::Bank.find(self.send(bank_code_method))
+        end
+      end
+    end
+  end
 end
--- spec/jp_bank_spec.rb
+++ spec/jp_bank_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe JpBank do
+  describe '.jp_bank' do
+    context 'Without any options' do
+      let(:klass) do
+        klass = Class.new do
+          include JpBank
+          jp_bank :bank
 
+          def bank_code
+            '0036'
+          end
+        end
+        klass
+      end
+
+      specify 'generates bank method' do
+        expect(klass.new.bank.code).to eq('0036')
+      end
+    end
+
+    context 'When specified method name by option' do
+      let(:klass) do
+        klass = Class.new do
+          include JpBank
+          jp_bank :bank, bank_code: :b_code
+
+          def b_code
+            '0036'
+          end
+        end
+        klass
+      end
+
+      specify 'generates bank method' do
+        expect(klass.new.bank.code).to eq('0036')
+      end
+    end
+  end
 end
